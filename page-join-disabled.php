a206064d54e5b11c91ecb5929bb75eef4b5eb2e2
emeraldjava <paul.t.oconnell@gmail.com>
2013-01-14 18:28:00 +0000

    drop the raw tags

--- page-join-disabled.php
+++ page-join-disabled.php
@@ -4,12 +4,18 @@
  */
 get_header();
 
+echo '<section>';
 // query for the membership page for its content
 $your_query = new WP_Query('pagename=join');
 while ( $your_query->have_posts() ) : $your_query->the_post();
 echo the_content();
 endwhile;
-wp_reset_postdata();
 
-echo do_shortcode('[alert type="general"]The online registration form is currently disabled while we prepare for the next event. Registration is available on the day at the next event[/alert]');
+echo '<div class="alert notice">';
+echo '<div class="msg">The online registration form is currently disabled while we prepare for the next event. Registration is available on the day at the next event</div>';
+echo '</div>';
+
+echo '</section>';
+
+get_footer();
 ?>
