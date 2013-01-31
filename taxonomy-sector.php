599bd7a400524f29c8364fcb5e65b7dd105ffcd2
emeraldjava <paul.t.oconnell@gmail.com>
2013-01-31 12:54:35 +0000

    Update the sector page

--- taxonomy-sector.php
+++ taxonomy-sector.php
@@ -15,36 +15,13 @@
 		<?php if (have_posts()) : ?>
 		<?php while(have_posts()): the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<?php
-			if($data['featured_images']):
-			if($data['legacy_posts_slideshow']) {
-				include('legacy-slideshow.php');
-			} else {
-				include('new-slideshow.php');
-			}
-			endif;
-			?>
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<div class="post-content">
-				<?php
-				if($data['content_length'] == 'Excerpt') {
-					the_excerpt('');
-				} else {
-					the_content('');
-				}
-				?>
-			</div>
-			<div class="meta-info">
-				<div class="alignleft">
-					<?php echo __('By', 'Avada'); ?> <?php the_author_posts_link(); ?><span class="sep">|</span><?php the_time('F jS, Y'); ?><span class="sep">|</span><?php the_category(', '); ?><span class="sep">|</span><?php comments_popup_link(__('0 Comments', 'Avada'), __('1 Comment', 'Avada'), '% '.__('Comments', 'Avada')); ?>
-				</div>
-				<div class="alignright">
-					<a href="<?php the_permalink(); ?>" class="read-more"><?php echo __('Read More', 'Avada'); ?></a>
-				</div>
-			</div>
+			<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 		</div>
 		<?php endwhile; ?>
-		<?php kriesi_pagination($pages = '', $range = 2); ?>
+		<?php 
+		// http://www.kriesi.at/archives/how-to-build-a-wordpress-post-pagination-without-plugin
+		kriesi_pagination($pages = '', $range = 2); 
+		?>
 		<?php else: ?>
 		<?php endif; ?>
 	</div>
