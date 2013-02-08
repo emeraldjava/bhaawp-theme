c8ea4cf2f1018a79d34346cb1f77a32a91fdb370
emeraldjava <paul.t.oconnell@gmail.com>
2013-02-08 18:07:37 +0000

    enable page options on the bhaa cpt's

--- framework/metaboxes.php
+++ framework/metaboxes.php
@@ -27,6 +27,9 @@ class PyreThemeFrameworkMetaboxes {
 	{
 		$this->add_meta_box('post_options', 'Post Options', 'post');
 		$this->add_meta_box('page_options', 'Page Options', 'page');
+		// enable page options on the bhaa cpt's
+		$this->add_meta_box('page_options', 'Page Options', 'event');
+		$this->add_meta_box('page_options', 'Page Options', 'house');
 		$this->add_meta_box('portfolio_options', 'Portfolio Options', 'avada_portfolio');
 	}
 	
