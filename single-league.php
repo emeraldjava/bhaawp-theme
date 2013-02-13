c4aa0216a87f2d36edc5a0c33e8aef84bbd123c3
emeraldjava <paul.t.oconnell@gmail.com>
2013-02-13 19:01:26 +0000

    basic league summary table

--- single-league.php
+++ single-league.php
@@ -10,10 +10,45 @@ $leagueSummary = new LeagueSummary(get_the_ID());
 
 echo '<h2>'.$leagueSummary->getName().''.get_the_ID().'</h2>';
 
-$summary=$leagueSummary->getLeageSummary();
-var_dump($summary,1);
+$leaguesummary=$leagueSummary->getLeageSummary();
 
-//echo get_the_ID();
+$i = 0;
+$division='';
+foreach($leaguesummary as $summary) :
+
+if($summary->leaguedivision!=$division)
+{
+	$i++;
+	echo '<h3>Division '.$summary->leaguedivision.'</h3>';
+	$division=$summary->leaguedivision;
+	
+	echo '<table>';
+	echo '<tr>
+    <th>Name</th>
+    <th>Points</th>
+	<th>Races</th>
+  	</tr>';
+}
+else
+{
+	// specific row
+	echo '<tr>
+    <td>'.$summary->display_name.'</td>
+    <td>'.$summary->leaguepoints.'</td>
+	<td>'.$summary->leaguescorecount.'</td>
+  	</tr>';
+}
+
+// close the table
+if($summary->leagueposition==10)
+{
+	echo '</table>';
+}
+
+endforeach;
+
+//var_dump($summary,1);
+
 endwhile;
 
 echo '</section>';
