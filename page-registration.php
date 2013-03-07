1a55d3728ae857313789048a9b8436cd951fd45e
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-07 20:26:43 +0000

    registration page without footer

---
+++ page-registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: BHAA Registration
+ */
+
+get_header();
+
+echo '<section id="primary">';
+do_shortcode('[bhaa_registration]');
+echo '</section>';
+
+//get_footer();
+?>
