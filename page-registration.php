fc7bbeb3dcffe6b17593cc055a94fea341b180f4
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-21 17:20:58 +0000

    apply filter to shortcode text

--- page-registration.php
+++ page-registration.php
@@ -6,8 +6,6 @@
 get_header();
 
 echo '<section id="primary">';
-do_shortcode('[bhaa_registration]');
+echo apply_filters('the_content',do_shortcode('[bhaa_registration]'));
 echo '</section>';
-
-//get_footer();
 ?>
