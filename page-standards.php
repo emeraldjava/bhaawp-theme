b6dab98b59484f79ff394e1c2677d1a150bce6ee
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-21 14:54:54 +0000

    Add distances to the table

--- page-standards.php
+++ page-standards.php
@@ -2,9 +2,55 @@
 /**
  * Template Name: BHAA Standards
  */
-global $EM_Event;
 get_header();
 echo '<section id="primary">';
+
+$distances = array();
+
+$distance = array();
+$distance['km'] = 1;
+$distance['title'] = '1km';
+$distances[0]= $distance;
+
+$distance = array();
+$distance['km'] = 1.6;
+$distance['title'] = '1Mile';
+$distances[1]= $distance;
+
+$distance = array();
+$distance['km'] = 5;
+$distance['title'] = '5km';
+$distances[2]= $distance;
+
+$distance = array();
+$distance['km'] = 8;
+$distance['title'] = '5Mile';
+$distances[3]= $distance;
+
+$distance = array();
+$distance['km'] = 10;
+$distance['title'] = '10km';
+$distances[4]= $distance;
+
+$distance = array();
+$distance['km'] = 21.1;
+$distance['title'] = 'Half';
+$distances[5]= $distance;
+
+$distance = array();
+$distance['km'] = 42.2;
+$distance['title'] = 'Marathon';
+$distances[6]= $distance;
+
+global $BHAA;
+
+$standardTable = $BHAA->standardCalculator->generateTableForDistances($distances);
+
+echo apply_filters(
+		'the_content','
+[tagline_box title="Standards" description="The BHAA standard is like a golf handicap 
+		and give a runner a target time to aim for at races."]'.$standardTable);
+
 echo '</section>';
 get_footer();
 ?>
