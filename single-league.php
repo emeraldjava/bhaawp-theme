b3b174b85eaef3b0dc251abdc5b986903c05a331
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-22 11:47:36 +0000

    getLeagueSummaryByDivision

--- single-league.php
+++ single-league.php
@@ -6,8 +6,10 @@ while ( have_posts() ) : the_post();
 echo '<h1>'.get_the_title().'</h1>';
 
 $leagueSummary = new LeagueSummary(get_the_ID());
+$divisions = $leagueSummary->getDivisions();
+//var_dump($divisions);
 
-echo '<p>After a long delay the league tables are nearly back. There might be an odd sum or two incorrect and we still have to update the Race Organiser points. Hopefully St Patrick will rub a bit of polish onto proceeding over the weekend.</p>';
+//echo '<p>After a long delay the league tables are nearly back. There might be an odd sum or two incorrect and we still have to update the Race Organiser points. Hopefully St Patrick will rub a bit of polish onto proceeding over the weekend.</p>';
 //echo '<h2>'.$leagueSummary->getName().''.get_the_ID().'</h2>';
 
 //var_dump(get_terms('division'));
@@ -15,51 +17,56 @@ echo '<p>After a long delay the league tables are nearly back. There might be an
 //$terms = get_the_terms( get_the_ID(), 'division' );
 //$taxonomies=get_taxonomies('','division');
 
-$terms = get_terms('division');
-echo '<ul>';
-foreach ($terms as $term) {
-	echo '<li><a href="'.get_term_link($term->slug, 'division').'">Division '.$term->name.'</a></li>';
-}
-echo '</ul>';
+// $terms = get_terms('division');
+// echo '<ul>';
+// foreach ($terms as $term) {
+// 	echo '<li><a href="'.get_term_link($term->slug, 'division').'">Division '.$term->name.'</a></li>';
+// }
+// echo '</ul>';
 
 //var_dump($taxonomies,1);
 
-// $leaguesummary=$leagueSummary->getLeagueSummary();
+$leagueSummaryByDivision=$leagueSummary->getLeagueSummaryByDivision();
+var_dump($leagueSummaryByDivision);
 
-// $i = 0;
-// $division='';
-// foreach($leaguesummary as $summary) :
+foreach($divisions as $division) :
+	echo $division->code;
+endforeach;
 
-// if($summary->leaguedivision!=$division)
-// {
-// 	$i++;
-// 	echo '<h3>Division '.$summary->leaguedivision.'</h3>';
-// 	$division=$summary->leaguedivision;
+$i = 0;
+$division='';
+foreach($leagueSummaryByDivision as $summary) :
+
+if($summary->leaguedivision!=$division)
+{
+	$i++;
+	echo '<h3>Division '.$summary->leaguedivision.'</h3>';
+	$division=$summary->leaguedivision;
 	
-// 	echo '<table>';
-// 	echo '<tr>
-//     <th>Name</th>
-//     <th>Points</th>
-// 	<th>Races</th>
-//   	</tr>';
-// }
-// else
-// {
-// 	// specific row
-// 	echo '<tr>
-//     <td>'.$summary->display_name.'</td>
-//     <td>'.$summary->leaguepoints.'</td>
-// 	<td>'.$summary->leaguescorecount.'</td>
-//   	</tr>';
-// }
+	echo '<table>';
+	echo '<tr>
+    <th>Name</th>
+    <th>Points</th>
+	<th>Races</th>
+  	</tr>';
+}
+else
+{
+	// specific row
+	echo '<tr>
+    <td>'.$summary->display_name.'</td>
+    <td>'.$summary->leaguepoints.'</td>
+	<td>'.$summary->leaguescorecount.'</td>
+  	</tr>';
+}
 
-// // close the table
-// if($summary->leagueposition==10)
-// {
-// 	echo '</table>';
-// }
+// close the table
+if($summary->leagueposition==10)
+{
+	echo '</table>';
+}
 
-// endforeach;
+endforeach;
 
 // //var_dump($summary,1);
 
