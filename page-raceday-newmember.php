156c903c6a57606e89bbbe4e819c2885f1c3d03b
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-02 16:04:19 +0100

    update the list of fields on the new member form

--- page-raceday-newmember.php
+++ page-raceday-newmember.php
@@ -42,86 +42,32 @@ get_header();
 <?php 
 include_once 'page-raceday-header.php';
 
-echo "BHAA NEW MEMBER";
+echo "<h3>BHAA New Member</h3>";
 
 if(isset($registrationSubmitted) && $registrationSubmitted == true) 
 {
-	echo '<div class="thanks">
-	<p>The runner has been registered.</p></div>';
+	echo '<div class="thanks"><p>The runner has been registered.</p></div>';
 }
 else
 {
 	// http://jqueryui.com/autocomplete/#custom-data
 	// http://stackoverflow.com/questions/11349205/jqueryui-autocomplete-custom-data-and-display
-	echo apply_filters('the_content',
-		'[one_third last="yes"]
-			<div class="navbar-search pull-left" align="left">
-			Full name or ID : <input size="40" type="text" placeholder="Name or ID" id="memberfilter"/>
-			[raw]<script type="text/javascript">
-jQuery(document).ready( 
-	function($){
-	var runners = '.file_get_contents("wp-content/bhaa_runners.json.txt").';
-	$("#memberfilter").autocomplete({
-		source: runners,
-		minLength: 3,
-		focus: function( event, ui ) {
-        	$( "#memberfilter" ).val( ui.item.label );
-        	return false;
-      	},
-		select: function(event, ui) {
-			$("#runner").val( ui.item.id );
-			$("#first").val( ui.item.firstname );
-			$("#last").val( ui.item.lastname );
-			$("#dob").val( ui.item.dob );
-			$("#company").val( ui.item.company );
-			$("#standard").val( ui.item.standard );
-			$("#gender").val( ui.item.gender );
-			return true;	
-		}
-	})
-	.data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-		return $("<li></li>")
-        	.data("item.autocomplete", item)
-        	.append("<a>"+item.label+" "+item.id+"</a><br/><small>DOB:"+item.dob+", Status:"+item.status+"</small>")
-			.appendTo(ul);
-    };
-});
-</script>[/raw]
-		</div>
-	[/one_third]');
-	
-	$races = $BHAA->registration->getNextRaces();
-	var_dump($races);
-	$selectRaces = '<select name="raceid">';
-	$i=0;
-	foreach($races as $race)
-	{
-		$rname = $race->dist.'-'.$race->unit.'-'.$race->type;
-		$selectRaces .= sprintf('<option value=%d>%s</option>',$race->id,$rname);// $race->id,$race->id,$rname);
-	}
-	$selectRaces .= '</select>';
-		
-	//var_dump('name'.$name);
-	//var_dump('get_permalink '.get_permalink());
-
 	echo apply_filters('the_content','
-		<form action="" id="bhaa-registration-form" method="POST">
+		<form action="" id="bhaa-raceday-newmember" method="POST">
 			[one_half last="no"]
-			<b>Race Details</b><br/>
-			RaceNumber<input type="text" name="number" id="number"/><br/>
-			Race'.$selectRaces.'<br/>
-			[/one_half]
-			[one_half last="yes"]
-			<b>Runner Details</b><br/>
-			ID<input type="text" name="runner" id="runner"/><br/>
+			<b>Runner Details - REQUIRED</b><br/>
 			Firstname<input type="text" name="first" id="first"/><br/>
 			Surname<input type="text" name="last" id="last"/><br/>
 			Gender<input type="text" name="gender" id="gender"/><br/>
 			DOB<input type="text" name="dob" id="dob"/><br/>
-			Standard<input type="text" name="standard" id="standard"/><br/>
-			Company<input type="text" name="company" id="company"/><br/>
 			[/one_half]
-			<input type="submit" value="Register Runner"/>
+			[one_half last="yes"]
+			<b>Extra Details</b><br/>
+			Email<input type="text" name="company" id="company"/><br/>
+			Mobile<input type="text" name="company" id="company"/><br/>
+			Company<input type="text" name="company" id="company"/><br/>
+			<input type="submit" value="Register New Runner"/>
+			[/one_half]
 			<input type="hidden" name="form-submitted" value="true" />
 		</form>');
 }
