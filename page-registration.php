44df2bc0ff081831984ce061a08a8722e45e8075
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-02 14:22:53 +0100

    clean up

--- page-registration.php
+++ page-registration.php
@@ -1,62 +1,61 @@
 <?php
 /**
  * Template Name: BHAA Registration
- * 
- * http://www.catswhocode.com/blog/how-to-create-a-built-in-contact-form-for-your-wordpress-theme
  */
 
+global $BHAA;
 
 if(isset($_POST['form-submitted'])) 
 {
-// 	if(trim($_POST['runner']) === '') {
-// 		$runnerError = 'Please enter runner runner.';
-// 		$hasError = true;
-// 	} else {
+ 	if(trim($_POST['runner']) === '') {
+ 		$runnerError = 'Please enter runner runner.';
+ 		$hasError = true;
+ 	} else {
  		$runner = trim($_POST['runner']);
-// 	}
+ 	}
 
-// 	if(trim($_POST['number']) === '')  {
-// 		$numberError = 'Please enter a race number.';
-// 		$hasError = true;
-// 	} else {
+ 	if(trim($_POST['number']) === '')  {
+ 		$numberError = 'Please enter a race number.';
+ 		$hasError = true;
+ 	} else {
  		$number = trim($_POST['number']);
-// 	}
+ 	}
 	
  	$raceid = trim($_POST['raceid']);
 
-// 	//if(!isset($hasError)) {
- 	error_log($raceid.' '.$runner.' '.$number);
-// 	global $BHAA;
-// 	$BHAA->registration->registerRunner($raceid,$runner,$number);
- 	//$registrationSubmitted = true;
-// 	//}
+	if(!isset($hasError))
+	{
+ 		error_log($raceid.' '.$runner.' '.$number);
+ 		$BHAA->registration->registerRunner($raceid,$runner,$number);
+ 		$registrationSubmitted = true;
+	}
 }
-global $BHAA;
+
 // http://stackoverflow.com/questions/11368368/404-when-using-post-get-parameters-in-wordpress-3-4
 get_header();
-echo "<pre>GET "; print_r($_GET); echo "</pre>";
-echo "<pre>POST "; print_r($_POST); echo "</pre>";
+//echo "<pre>GET "; print_r($_GET); echo "</pre>";
+//echo "<pre>POST "; print_r($_POST); echo "</pre>";
 ?>
 
 <div id="container">
 	
-	<?php 
-	include_once 'registration-header.php';
+<?php 
+include_once 'registration-header.php';
 
-	if(isset($registrationSubmitted) && $registrationSubmitted == true) 
-	{
-		echo '<div class="thanks">
- 			<p>The runner has been registered.</p></div>';
-	}
-	else
-	{
-		// http://jqueryui.com/autocomplete/#custom-data
-		// http://stackoverflow.com/questions/11349205/jqueryui-autocomplete-custom-data-and-display
-		echo apply_filters('the_content',
-			'[one_third last="yes"]
-				<div class="navbar-search pull-left" align="left">
-				Full name or ID : <input size="40" type="text" placeholder="Name or ID" id="memberfilter"/>
-				[raw]<script type="text/javascript">
+if(isset($registrationSubmitted) && $registrationSubmitted == true) 
+{
+	echo '<div class="thanks">
+	<p>The runner has been registered.</p></div>';
+}
+else
+{
+	// http://jqueryui.com/autocomplete/#custom-data
+	// http://stackoverflow.com/questions/11349205/jqueryui-autocomplete-custom-data-and-display
+	echo apply_filters('the_content',
+		'[one_third last="yes"]
+			<div class="navbar-search pull-left" align="left">
+			Full name or ID : <input size="40" type="text" placeholder="Name or ID" id="memberfilter"/>
+			[raw]<script type="text/javascript">
 jQuery(document).ready( 
 	function($){
 	var runners = '.file_get_contents("wp-content/bhaa_runners.json.txt").';
@@ -86,61 +85,44 @@ jQuery(document).ready(
     };
 });
 </script>[/raw]
-				</div>
-			[/one_third]');
-		
-		$races = $BHAA->registration->getNextRaces();
-		var_dump($races);
-		$selectRaces = '<select name="raceid">';
-		$i=0;
-		foreach($races as $race)
-		{
-			$rname = $race->dist.'-'.$race->unit.'-'.$race->type;
-			$selected="";
-			if($i==0)
-			{
-//				$selected='selected="selected"';
-				//$selectRaces .= sprintf('<option value=%d selected="selected">%s</option>',$i++,$rname);
-			}
-			else
-			{
-				//$selectRaces .= sprintf('<option value=%d>%s</option>',$i++,$rname);
-			}
-			// http://stackoverflow.com/questions/5189645/how-to-remember-chosen-form-values-in-php
-			// http://wordpress.stackexchange.com/questions/85607/theme-option-select-values
-			//var_dump('$racez->id '.trim($racez->id));
-			//var_dump(sprintf('%s - %s - %d',$racez->id,trim($racez->id),$racez->id));
-//			$i = intval($racez->id);
-//			$rr = (int) $racez->id;
-			
-			$selectRaces .= sprintf('<option value=%d>%s</option>',$race->id,$rname);// $race->id,$race->id,$rname);
-		}
-		$selectRaces .= '</select>';
-		
-		var_dump('name'.$name);
-		var_dump('get_permalink '.get_permalink());
-		
-		echo apply_filters('the_content','
-			<form action="" id="bhaa-registration-form" method="POST">
-				[one_half last="no"]
-				<b>Race Details</b><br/>
-				RaceNumber<input type="text" name="number" id="number"/><br/>
-				Race'.$selectRaces.'<br/>
-				[/one_half]
-				[one_half last="yes"]
-				<b>Runner Details</b><br/>
-				ID<input type="text" name="runner" id="runner"/><br/>
-				Firstname<input type="text" name="first" id="first"/><br/>
-				Surname<input type="text" name="last" id="last"/><br/>
-				Gender<input type="text" name="gender" id="gender"/><br/>
-				DOB<input type="text" name="dob" id="dob"/><br/>
-				Standard<input type="text" name="standard" id="standard"/><br/>
-				Company<input type="text" name="company" id="company"/><br/>
-				[/one_half]
-				<input type="submit" value="Register Runner"/>
-				<input type="hidden" name="form-submitted" value="true" />
-			</form>');
+		</div>
+	[/one_third]');
+	
+	$races = $BHAA->registration->getNextRaces();
+	var_dump($races);
+	$selectRaces = '<select name="raceid">';
+	$i=0;
+	foreach($races as $race)
+	{
+		$rname = $race->dist.'-'.$race->unit.'-'.$race->type;
+		$selectRaces .= sprintf('<option value=%d>%s</option>',$race->id,$rname);// $race->id,$race->id,$rname);
 	}
-	echo '</div>';
+	$selectRaces .= '</select>';
+		
+	//var_dump('name'.$name);
+	//var_dump('get_permalink '.get_permalink());
+
+	echo apply_filters('the_content','
+		<form action="" id="bhaa-registration-form" method="POST">
+			[one_half last="no"]
+			<b>Race Details</b><br/>
+			RaceNumber<input type="text" name="number" id="number"/><br/>
+			Race'.$selectRaces.'<br/>
+			[/one_half]
+			[one_half last="yes"]
+			<b>Runner Details</b><br/>
+			ID<input type="text" name="runner" id="runner"/><br/>
+			Firstname<input type="text" name="first" id="first"/><br/>
+			Surname<input type="text" name="last" id="last"/><br/>
+			Gender<input type="text" name="gender" id="gender"/><br/>
+			DOB<input type="text" name="dob" id="dob"/><br/>
+			Standard<input type="text" name="standard" id="standard"/><br/>
+			Company<input type="text" name="company" id="company"/><br/>
+			[/one_half]
+			<input type="submit" value="Register Runner"/>
+			<input type="hidden" name="form-submitted" value="true" />
+		</form>');
+}
+echo '</div>';
 ?>
 <?php get_footer(); ?>
