032abf3d62fc31b1917b15e562c2e05e04c2e329
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-03 19:14:42 +0100

    html table the list

--- page-raceday-list.php
+++ page-raceday-list.php
@@ -11,7 +11,33 @@ get_header();
 
 include_once 'page-raceday-header.php';
 
-echo var_dump($BHAA->registration->listRegisteredRunners());
 
+echo '<div id="raceteclist">
+<div class="row">
+<span class="cell">Event</span>
+<span class="cell">Race Number</span>
+<span class="cell">BHAA</span>
+<span class="cell">Name</span>
+<span class="cell">Standard</span>
+<span class="cell">Company</span>
+<span class="cell">Team</span>
+</div>';
+
+$racetec = $BHAA->registration->listRegisteredRunners();
+
+foreach($racetec as $racetec) : ?>
+<div class="row">
+<span class="cell"><?php echo $racetec->race;?></span>
+<span class="cell"><?php echo $racetec->racenumber;?></span>
+<span class="cell"><?php echo $racetec->runner;?></span>
+<span class="cell"><?php echo $racetec->firstname;?> <?php echo $racetec->lastname;?></span>
+<span class="cell"><?php echo $racetec->standard;?></span>
+<span class="cell"><?php echo $racetec->companyname;?></span>
+<span class="cell"><?php echo $racetec->teamname;?></span>
+</div>
+<?php endforeach;?>
+</div>
+
+<?php 
 get_footer(); 
 ?>
