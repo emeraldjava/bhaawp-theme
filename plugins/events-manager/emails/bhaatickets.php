47e960775259ea00323093fe84e53b15c5e8f1fc
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-10 18:35:07 +0100

    display the company name in the email

--- plugins/events-manager/emails/bhaatickets.php
+++ plugins/events-manager/emails/bhaatickets.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html><head><title>BHAA Online Ticket : #_BOOKINGNAME</title></head>
+
 <body><div style="">
-<table cellspacing="0" cellpadding="8" border="0" summary="" style="width: 100%; font-family: Arial, Sans-serif; 
+<table cellspacing="0" cellpadding="1" border="0" summary="" style="width: 100%; font-family: Arial, Sans-serif; 
 	border: 1px Solid #ccc; border-width: 1px 2px 2px 1px; background-color: #fff;">
 <tr>
 <td>
@@ -31,26 +32,24 @@ else
 	$membershipDetails = false;
 }
 
-echo '<table cellpadding="0" cellspacing="0" border="0" summary="details">';
+echo '<table cellpadding="1" cellspacing="1" border="1" summary="details">';
 
-echo '<tr><td span=1 style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
+echo '<tr><td style="padding: 0 1em 1px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
 echo '<img class="bhaa-logo" src="http://bhaa.ie/wp-content/uploads/2012/11/headerlogo.jpg" width="97" height="100" alt="BHAA Logo" style="float: left; padding: 20px 20px" />';
-echo '</tr>';
-
-echo '<tr><td span=2 style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
-echo '<h3 style="padding:0 0 6px 0;margin:0;font-family:Arial,Sans-serif;font-size:16px;font-weight:bold;color:#222">'.$header.'</h3>';
+echo '<td style="padding: 0 1em 1px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
+echo '<h3 style="padding:0 0 6px 0;margin:0;font-family:Arial,Sans-serif;font-size:16px;font-weight:bold;color:#222">'.$header.'</h3></td>';
 echo '</tr>';
 ?>
 
 <!-- Who -->
 <tr>
-<td	style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
+<td	style="padding: 0 1em 1px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
 <div><i style="font-style: normal">Name</i></div></td>
 <td	style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_BOOKINGNAME</td>
 </tr>
 
 <?php
-echo '<tr><td style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
+echo '<tr><td style="padding: 0 1em 1px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
 echo '<div><i style="font-style: normal">BHAA ID</i></div></td>';
 echo '<td style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_BHAAID</td>';
 echo '</tr>';
@@ -58,14 +57,14 @@ echo '</tr>';
 
 <!-- Date Of Birth -->
 <tr>
-<td	style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
+<td	style="padding: 0 1em 1px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
 <div><i style="font-style: normal">Date Of Birth</i></div></td>
 <td	style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_BOOKINGFORMCUSTOM{bhaa_runner_dateofbirth}</td>
 </tr>
 
 <!-- Gender -->
 <tr>
-<td	style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
+<td	style="padding: 0 1em 1px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
 <div><i style="font-style: normal">Gender</i></div></td>
 <td	style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_BOOKINGFORMCUSTOM{bhaa_runner_gender}</td>
 </tr>
@@ -74,7 +73,7 @@ echo '</tr>';
 <tr>
 <td	style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">
 <div><i style="font-style: normal">Company</i></div></td>
-<td	style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_BOOKINGFORMCUSTOM{bhaa_runner_company_name}</td>
+<td	style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_BOOKINGFORMCUSTOM{bhaa_runner_companyname} - ID:#_BOOKINGFORMCUSTOM{bhaa_runner_company}</td>
 </tr>
 
 
@@ -82,13 +81,13 @@ echo '</tr>';
 // <!-- Thank you for registering for the BHAA #_EVENTLINK event.  -->
 	
 //<!-- When -->
-echo '<tr><td	style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
+echo '<tr><td style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
 echo '<div><i style="font-style: normal">When</i></div></td>';
 echo '<td style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_EVENTDATES @ #_EVENTTIMES</td>';
 echo '</tr>';
 
 //<!-- Where -->
-echo '<tr><td	style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
+echo '<tr><td style="padding: 0 1em 10px 0; font-family: Arial, Sans-serif; font-size: 13px; color: #888; white-space: nowrap" valign="top">';
 echo '<div><i style="font-style: normal">Where</i></div></td>';
 echo '<td style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">#_LOCATIONNAME - #_LOCATIONFULLLINE</td>';
 echo '</tr>';
@@ -111,7 +110,7 @@ echo '</tr>';
 
 <?php if($membershipDetails) {
 //<!-- Notes -->
-echo '<td rowspan="2" style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">
+echo '<tr><td colspan="2" style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">
 <p>
 Thank you for renewing your membership online.
 </p>
@@ -120,13 +119,12 @@ Thank you for renewing your membership online.
 <li>Your race results will be counted towards the BHAA league.</li>
 <li>You can run with your company or sector team.</li>
 </ul>
-</td>';
-echo '</tr>';
+</td></tr>';
 }
 ?>
 
 <?php if($eventDetails) {
-echo '<tr><td rowspan="2" style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">
+echo '<tr><td colspan="2" style="padding-bottom: 10px; font-family: Arial, Sans-serif; font-size: 13px; color: #222" valign="top">
 <p>
 Thank you for using the BHAA Day Membership form.
 </p>
@@ -137,8 +135,7 @@ Thank you for using the BHAA Day Membership form.
 <li>The BHAA is totally voluntary organisation.</li>
 <li><b style="color: red">You MUST PRINT and bring this email to the event registration.</b><li>
 </ul>
-</td>';
-echo '</tr>';
+</td></tr>';
 }
 ?>
 
