5b91233c3610d0e8fdec03e30e72edc72aaeec37
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-16 17:12:49 +0100

    create a two level menu

--- page-raceday-header.php
+++ page-raceday-header.php
@@ -1,8 +1,12 @@
 <?php 
 echo apply_filters(
 'the_content',
-'[one_fourth last="no"]<a href="/raceday-register/">Register</a>[/one_fourth]
-[one_fourth last="no"]<a href="/raceday-newmember/">New/Day Member</a>[/one_fourth]
-[one_fourth last="no"]<a href="/raceday-list/">List</a>[/one_fourth]
-[one_fourth last="yes"]<a href="/raceday-export/">Export</a>[/one_fourth]<hr/>');
+'[one_third last="no"]<a href="/raceday-register/">Register</a>[/one_third]
+[one_third last="no"]<a href="/raceday-newmember/">New/Day Member</a>[/one_third]
+[one_third last="yes"]<a href="/raceday-prereg/">Pre-Registered</a>[/one_third]
+[one_third last="no"]<a href="/raceday-list/">List</a>[/one_third]
+[one_third last="no"]<a href="/raceday-latest/">Latest</a>[/one_third]
+[one_third last="yes"]<a href="/raceday-export/">Export</a>[/one_third]
+<hr/>'
+);
 ?>
