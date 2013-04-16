3ce9e6c0e1ee3f17175e09964d46630379143f59
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-16 17:48:17 +0100

    add form to assign race numbers

--- page-raceday-prereg.php
+++ page-raceday-prereg.php
@@ -16,24 +16,27 @@ $racetec = $BHAA->registration->listPreRegisteredRunners();
 
 echo '<table id="raceteclist" >
 <tr class="row">
-<th class="cell">Race</th>
-<th class="cell">Number</th>
-<th class="cell">BHAA</th>
+<th class="cell">BHAA ID</th>
 <th class="cell">Name</th>
-<th class="cell">Standard</th>
+<th class="cell">DOB</th>
 <th class="cell">Company</th>
-<th class="cell">Team</th>
+<th class="cell">Assign Race Number</th>
 </tr>';
 
 foreach($racetec as $racetec) : ?>
 <tr class="row">
-<td class="cell"><?php echo $racetec->race;?></td>
-<td class="cell"><?php echo $racetec->racenumber;?></td>
 <td class="cell"><?php echo $racetec->runner;?></td>
 <td class="cell"><?php echo $racetec->firstname;?> <?php echo $racetec->lastname;?></td>
-<td class="cell"><?php echo $racetec->standard;?></td>
+<td class="cell"><?php echo $racetec->dateofbirth;?></td>
 <td class="cell"><?php echo $racetec->companyname;?></td>
-<td class="cell"><?php echo $racetec->teamname;?></td>
+<td class="cell">
+<form>
+	<input type="number" name="number"/>
+	<input type="hidden" name="raceid" value="2597">
+	<input type="hidden" name="runner" value="<?php echo $racetec->runner;?>">
+	<input type="submit" value="<?php echo $racetec->firstname;?> <?php echo $racetec->lastname;?> Race Number"/>
+</form>
+</td>
 </tr>
 <?php endforeach;?>
 </table>
