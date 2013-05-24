c87075dbb45aa1593749e0d58d97e4e83852750a
emeraldjava <paul.t.oconnell@gmail.com>
2013-05-24 17:35:31 +0100

    do the sums

--- page-raceday-cash.php
+++ page-raceday-cash.php
@@ -9,10 +9,7 @@ if ( !current_user_can( 'edit_users' ) )  {
 get_header();
 
 global $BHAA;
-
 $runnerCount = $BHAA->registration->getRegistrationTypes(2849);
-
-var_dump($runnerCount,true);
 
 /**
  * 1 - member - 10e
@@ -31,22 +28,52 @@ $new=0;
 $online_day=0;
 $online_member=0;
 
+$total=0;
+$ro=0;
+$bhaa=0;
+$online=0;
 foreach($runnerCount as $runner){
 	switch($runner->type){
-		case(1):
+		case(1):{
 			$member=$runner->count;
-		case(2):			
+			$total = $total + ($member*10);
+			$ro = $ro + ($member*10);
+		}
+		case(2):{
 			$inactive_day=$runner->count;
-		case(3):
+			$total = $total + ($inactive_day*15);
+			$ro = $ro + ($inactive_day*10);
+			$bhaa = $bhaa + ($inactive_day*5);
+		}
+		case(3):{
 			$renew=$runner->count;
-		case(4):
+			$total = $total + ($renew*25);
+			$ro = $ro + ($renew*10);
+			$bhaa = $bhaa + ($renew*15);
+		}
+		case(4):{
 			$day=$runner->count;
-		case(5):
+			$total = $total + ($day*15);
+			$ro = $ro + ($day*10);
+			$bhaa = $bhaa + ($day*5);
+		}
+		case(5):{
 			$new=$runner->count;
-		case(6):
+			$total = $total + ($new*25);
+			$ro = $ro + ($new*10);
+			$bhaa = $bhaa + ($new*15);
+		}
+		case(6):{
 			$online_day=$runner->count;
-		case(7):
+			$online = $online + ($online_day*15);
+			$ro = $ro + ($online_day*10);
+			//$bhaa = $bhaa + ($online_day*5);
+		}
+		case(7):{
 			$online_member=$runner->count;
+			$online = $online + ($online_day*10);
+			$ro = $ro + ($online_member*10);
+		}
 	}
 }
 
@@ -57,6 +84,10 @@ echo 'day '.$day.'<br/>';
 echo 'new '.$new.'<br/>';
 echo 'online_day '.$online_day.'<br/>';
 echo 'online_member '.$online_member.'<br/>';
+echo '<hr/>Total '.$total.'<br/>';
+echo 'Race organiser '.$ro.'<br/>';
+echo 'BHAA '.$bhaa.'<br/>';
+echo 'Online '.$online.'<br/>';
 
 get_footer(); 
 ?>
